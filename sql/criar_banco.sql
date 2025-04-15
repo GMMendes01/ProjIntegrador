796997798a96a2244b26679131a7be42584b20e5
GMMendes01 <g7000mendes@gmail.com>
2025-04-15 09:21:54 -0300

    fix-sql1.0

--- sql/criar_banco.sql
+++ sql/criar_banco.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS banco2;
 use banco2;
 
 CREATE table cadastro(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50),
     sobrenome VARCHAR(50),
     email VARCHAR(100),
--- telas/cadastro.php
+++ telas/cadastro.php
@@ -11,7 +11,7 @@ $sql="INSERT INTO cadastro(nome, sobrenome, email, senha)
 VALUES ('$nome', '$sobrenome', '$email', '$senha')";
 
 if(mysqli_query($conn, $sql)){
-    echo "Usuario conectado";
+    echo "\nUsuario cadastrado";
     include("navbar.html");/*inclui a pagina de pós registro (ainda nao está criada)*/ 
 }else{
     echo "Erro".mysqli_connect_error($conn);
