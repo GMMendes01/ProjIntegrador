33613b3004de9b8dff62b4ceea75fc8ec3214e18
GMMendes01 <g7000mendes@gmail.com>
2025-04-30 12:33:37 -0300

    teste backbtn

--- telas/index.php
+++ telas/index.php
@@ -11,6 +11,25 @@
 <?php 
 include('navbar.html');
     ?>
+    <button class="button" id="backBtn">
+  <div class="button-box">
+    <span class="button-elem">
+      <svg viewBox="0 0 46 40" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M46 20.038c0-.7-.3-1.5-.8-2.1l-16-17c-1.1-1-3.2-1.4-4.4-.3-1.2 1.1-1.2 3.3 0 4.4l11.3 11.9H3c-1.7 0-3 1.3-3 3s1.3 3 3 3h33.1l-11.3 11.9c-1 1-1.2 3.3 0 4.4 1.2 1.1 3.3.8 4.4-.3l16-17c.5-.5.8-1.1.8-1.9z"
+        ></path>
+      </svg>
+    </span>
+    <span class="button-elem">
+      <svg viewBox="0 0 46 40">
+        <path
+          d="M46 20.038c0-.7-.3-1.5-.8-2.1l-16-17c-1.1-1-3.2-1.4-4.4-.3-1.2 1.1-1.2 3.3 0 4.4l11.3 11.9H3c-1.7 0-3 1.3-3 3s1.3 3 3 3h33.1l-11.3 11.9c-1 1-1.2 3.3 0 4.4 1.2 1.1 3.3.8 4.4-.3l16-17c.5-.5.8-1.1.8-1.9z"
+        ></path>
+      </svg>
+    </span>
+  </div>
+</button>
+
     <div class="container">
 
  <img name="img-principal" src="../img/imagem1.png" alt="">
@@ -22,6 +41,9 @@ include('navbar.html');
  <h1>Como reciclar</h1>
  <img name="img-1" src="../img/47A7FFD8-2DCE-4040-A80D-1255C26BB7F9.png" alt="">
  
+ 
+ 
+
 </div>
 
 
