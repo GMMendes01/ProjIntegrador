be6b034298d47299620973c7d60014ad1fff76eb
GMMendes01 <g7000mendes@gmail.com>
2025-05-05 14:52:21 -0300

    codbarras front50%

--- telas/coddigito.php
+++ telas/coddigito.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/stylecodbarras.css">
+    <title>EcoPoints - Registrar pontos</title>
 </head>
 <body>
+    <label for="codbarras">Digite o codigo de barras</label>
     <input type="number" name="CODbarras" id="CODbarras">
+    <label for="qnt">Quantidade</label>
+    <input type="number">
 </body>
 </html>
