fb34798ebd648c5fb74a3ed36b748eb9d67a8af2
GMMendes01 <g7000mendes@gmail.com>
2025-05-17 11:00:12 -0300

    [feature] passos-add

--- telas/index.php
+++ telas/index.php
@@ -3,14 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Font Awesome -->
-<link
-  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-  rel="stylesheet"
-/>
-<!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
     <link rel="stylesheet" href="../css/styleindex.css">
+     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
+  <!-- MDB -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.2.0/mdb.min.css" rel="stylesheet"/>
     
     <title>EcoPoints</title>
 </head>
@@ -25,23 +21,37 @@ include('navbar.html');
  <img name="img-principal" src="../img/imagem1.png" alt="">
 
  <div class="p1">
- <p>Transforme seus resíduos em recompensas com o EcoPoints! 🌱♻️ Cada ação sustentável conta: recicle, acumule pontos e troque por benefícios incríveis enquanto ajuda a preservar o planeta. Junte-se a essa revolução verde e faça a diferença hoje mesmo – porque cuidar do meio ambiente nunca foi tão gratificante!</p>
+ <h1>Como reciclar</h1>
  </div>
  <br>
  
- <h1>Como reciclar</h1>
+ <h1></h1>
 
  <div class="how-to-recycle">
     <div class="img-txt1">
  <img name="img-1" src="../img/47A7FFD8-2DCE-4040-A80D-1255C26BB7F9.png" alt="">
  <div id="passo1">
-    <h5>Passo 1 - Separação</h6><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo autem eum ipsa praesentium expedita exercitationem suscipit ullam illum architecto laboriosam rem minus, voluptatem sequi deserunt necessitatibus eius quaerat voluptatum optio.</p></div>
+    <h5>Passo 1 - Separação</h6><p>Antes de descartar, classifique corretamente seus materiais recicláveis:<br>
+✅ Aceitos: Plástico, vidro, metal, papel, eletrônicos e óleo de cozinha. <br>
+❌ Não aceitos: Lixo orgânico ou resíduos contaminados.<br>
+Dica: Lave embalagens para evitar mau cheiro e facilitar a reciclagem!</p></div>
     </div>
     <br>
  
     <div class="img-txt2">
     <div id="passo2">
-    <h5>Passo 2 - Seilaoq</h6><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo autem eum ipsa praesentium expedita exercitationem suscipit ullam illum architecto laboriosam rem minus, voluptatem sequi deserunt necessitatibus eius quaerat voluptatum optio.</p></div>
+    <h5>Passo 2 - Ache um ponto proximo a você</h6><p>Use o mapa inteligente do Eco Points para localizar ecopontos, cooperativas ou máquinas de coleta automática:
+📍 Como fazer: <br>
+
+Acesse a aba "Pontos de Coleta" no app ou site. <br>
+
+Ative seu GPS ou digite seu endereço. <br>
+
+Escolha o local mais conveniente (indicamos o mais próximo com tempo de deslocamento). <br>
+
+Benefício: Pontos extras por escolher locais a menos de 1km da sua casa! 
+
+</p></div>
      <img name="img-1" src="../img/47A7FFD8-2DCE-4040-A80D-1255C26BB7F9.png" alt="">
     </div>
     <br>
@@ -49,11 +59,25 @@ include('navbar.html');
     <div class="img-txt1">
  <img name="img-1" src="../img/47A7FFD8-2DCE-4040-A80D-1255C26BB7F9.png" alt="">
  <div id="passo3">
-    <h5>Passo 3 - Separação</h6><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo autem eum ipsa praesentium expedita exercitationem suscipit ullam illum architecto laboriosam rem minus, voluptatem sequi deserunt necessitatibus eius quaerat voluptatum optio.</p></div>
+    <h5>Passo 3 - Leve ao ponto e colete os pontos</h6><p>Valide sua ação sustentável de duas formas: <br>
+🔹 Opção 1: Leia o QR Code do ecoponto com seu celular. <br>
+🔹 Opção 2: Tire uma foto do material descartado no app (tecnologia de reconhecimento de imagem). <br>
+
+✔️ Pontuação Instantânea: <br>
+
+50 pts/kg para plástico/metal <br>
+
+30 pts/kg para papel/vidro <br>
+
+100 pts/litro para óleo</p></div>
     </div>
     <br>
 
-    <footer class="bg-body-tertiary text-center">
+    
+
+
+</div>
+<footer class="bg-body-tertiary text-center">
   <!-- Grid container -->
   <div class="container p-4 pb-0">
     <!-- Section: Social media -->
@@ -112,7 +136,7 @@ include('navbar.html');
         data-mdb-ripple-init
         class="btn text-white btn-floating m-1"
         style="background-color: #333333;"
-        href="#!"
+        href="https://github.com/GMMendes01/ProjIntegrador"
         role="button"
         ><i class="fab fa-github"></i
       ></a>
@@ -123,15 +147,11 @@ include('navbar.html');
 
   <!-- Copyright -->
   <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-    © 2020 Copyright:
-    <a class="text-body" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    © 2025 Copyright:
+    <a class="text-body" href="#">EcoPoints.com</a>
   </div>
   <!-- Copyright -->
 </footer>
-
-
-</div>
-
 
 <script type="text/javascript"src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/9.0.0/mdb.umd.min.js"></script>
    
